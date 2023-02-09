57b765bcbebcc5523d28663d4aa7fbda36572b8d
kphan20 <81403321+kphan20@users.noreply.github.com>
2023-02-09 11:25:23 -0500

    minor change

--- algorithms/opt_astar.py
+++ algorithms/opt_astar.py
@@ -50,7 +50,7 @@ def astar(maze, start, end, state, heuristic=None):
         curr_orientation = current_node.pacbot_orientation % 2
         turned = False
         for direction, new_position in enumerate(
-            [(-1, 0), (0, -1), (1, 0), (0, 1)]
+            ((-1, 0), (0, -1), (1, 0), (0, 1))
         ):  # Adjacent squares
 
             # Get node position
